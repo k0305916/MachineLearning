1a3a0c0521065caa0b048642d10b7f47aed5f845
李繁 <lifan@Mr-Li-iMac.local>
2018-09-11 22:31:05 +0800

    fix a error.

--- PythonLesson/main.py
+++ PythonLesson/main.py
@@ -71,7 +71,7 @@ def Plot_decision_regions(x,y,classifier,resolution=0.02):
 
     x1_min,x1_max = x[:,0].min() -1,x[:,0].max()+1
     x2_min,x2_max = x[:,1].min() -1,x[:,1].max()+1
-    xx1,xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x1_min,x1_max,resolution))
+    xx1,xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
     z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
     z = z.reshape(xx1.shape)
     plt.contourf(xx1,xx2,z,alpha=0.4,cmap=cmap)
