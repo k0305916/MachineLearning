14dca7d556e27d20641921d2cf7e1809f6b70835
k0305916 <k03059164@sina.com>
2018-09-12 13:25:10 +0800

    format by autopep8.

--- PythonLesson/main.py
+++ PythonLesson/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+
 class Perception(object):
     '''
     Perception classifier
@@ -22,12 +23,12 @@ class Perception(object):
         Number of misclassifications in every epoch
     '''
 
-    def __init__(self,eta=0.1,n_iter=10):
+    def __init__(self, eta=0.1, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
         self.errors = []
 
-    def fit(self,x,y):
+    def fit(self, x, y):
         '''Fit training data.
 
         Parameters
@@ -48,62 +49,67 @@ class Perception(object):
 
         for _ in range(self.n_iter):
             errors = 0
-            for xi, target in zip(x,y):
+            for xi, target in zip(x, y):
                 update = self.eta * (target - self.predict(xi))
-                self.w_[1:]+= update * xi
+                self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
 
-    def net_input(self,x):
+    def net_input(self, x):
         '''Calculate net input'''
-        return np.dot(x,self.w_[1:]) + self.w_[0]
+        return np.dot(x, self.w_[1:]) + self.w_[0]
 
-    def predict(self,x):
+    def predict(self, x):
         '''Return class label atfer unit step'''
-        return np.where(self.net_input(x) >= 0.0,1,-1)
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
 
-def Plot_decision_regions(x,y,classifier,resolution=0.02):
-    markers=('s','x','o','^','v')
-    colors = ('red','blue','lightgreen','gray','cyan')
+
+def Plot_decision_regions(x, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
-    x1_min,x1_max = x[:,0].min() -1,x[:,0].max()+1
-    x2_min,x2_max = x[:,1].min() -1,x[:,1].max()+1
-    xx1,xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
-    z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    x1_min, x1_max = x[:, 0].min() - 1, x[:, 0].max()+1
+    x2_min, x2_max = x[:, 1].min() - 1, x[:, 1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     z = z.reshape(xx1.shape)
-    plt.contourf(xx1,xx2,z,alpha=0.4,cmap=cmap)
-    plt.xlim(xx1.min(),xx1.max())
-    plt.ylim(xx2.min(),xx2.max())
+    plt.contourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
 
-    for idx,cl in enumerate(np.unique(y)):
-        plt.scatter(x=x[y==cl,0],y=x[y==cl,1],alpha=0.8,c=cmap(idx),marker=markers[idx],label = cl)
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=x[y == cl, 0], y=x[y == cl, 1], alpha=0.8,
+                    c=cmap(idx), marker=markers[idx], label=cl)
 
 
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
+df = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 df.tail()
 
-y = df.iloc[0:100,4].values
-y = np.where(y == 'Iris-setosa',-1,1)
-x = df.iloc[0:100,[0,2]].values
-plt.scatter(x[:50,0],x[:50,1],color='red',marker='o',label='setosa')
-plt.scatter(x[50:100,0],x[50:100,1],color='blue',marker='x',label='versicolor')
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+x = df.iloc[0:100, [0, 2]].values
+plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
+            marker='x', label='versicolor')
 plt.xlabel('petal length')
 plt.ylabel('sepal length')
 plt.legend(loc='upper left')
 
 # plt.show()
 
-ppn = Perception(eta=0.1,n_iter=10)
-ppn.fit(x,y)
-plt.plot(range(1,len(ppn.errors_)+1),ppn.errors_,marker='o')
+ppn = Perception(eta=0.1, n_iter=10)
+ppn.fit(x, y)
+plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of misclassifications')
 # plt.show()
 
-Plot_decision_regions(x,y,classifier=ppn)
+Plot_decision_regions(x, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
