0af8744861ff8cf4cc7aef77d575537978122806
k0305916 <k03059164@sina.com>
2018-09-27 16:24:38 +0800

    Section 2 result.

--- PythonLesson/2.2/basicMCP.py
+++ PythonLesson/2.2/basicMCP.py
@@ -48,24 +48,26 @@ class Perception(object):
         self.errors_ = []
 
         for _ in range(self.n_iter):
+            print(self.w_)
             errors = 0
-            # 1. This is 1D weight array.
+            # 1. This is 1D weight array.--error. This is 2D weight array.
             # 2. 'update' always will update weight.
             for xi, target in zip(x,y):
                 update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
-                self.w_[0] += update
+                #This code can delete because it just work for the total.
+                # self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
 
     def net_input(self, x):
         '''Calculate net input'''
-        print(np.dot(x,self.w_[1:]) + self.w_[0])
         return np.dot(x,self.w_[1:]) + self.w_[0]
 
     def predict(self, x):
         '''Return class label atfer unit step'''
+        #单位阶跃函数: work for predict output and update weight.
         return np.where(self.net_input(x) >= 0.0, 1, -1)
 
 
@@ -89,86 +91,25 @@ def Plot_decision_regions(x, y, classifier, resolution=0.02):
                     c=cmap(idx), marker=markers[idx], label=cl)
 
 
-class AdalineGD(object):
-    '''
-    ADAptive Linear Neuron classifier.
-
-    Parameters:
-    eta: float
-        Learning rate(between 0.0 and 1.0)
-    n_iter: int
-        Passes over the training dataset.
-
-    Attributes:
-    w_: id-array.
-        Weights after fitting.
-    
-    errors: list
-        Number of misclassifications in every epoch.
-    '''
-
-    def __init__(self, eta = 0.01, n_iter = 50):
-        self.eta = eta
-        self.n_iter = n_iter
-
-    def fit(self,x,y):
-        '''
-        Fit training data
-
-        Parameters
-        ---------
-        x: (Array-like),shape = [n_samples, n_features]
-            Training vectors, where n_samples is the number of samples and 
-            n_features is the number of features.
-
-        y : array-like, Shape = [n_samples]
-            Target values.
-
-        Returns
-        ------
-        self : object
-        '''
-        self.w_ = np.zeros(1+x.shape[1])
-        self.cost_ = []
-
-        for i in range(self.n_iter):
-            output = self.net_input(x)
-            errors = (y-output)
-            self.w_[1:] += self.eta * x.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = (errors**2).sum() / 2.0
-            self.cost_.append(cost)
-        return self
-
-    def net_input(self,x):
-        '''
-        Calculate net input
-        '''
-        return np.dot(x,self.w_[1:] + self.w_[0])
-
-    def activation(self,x):
-        '''Compute linear activation'''
-        return self.net_input(x)
-
-    def predict(self,x):
-        '''Return class label after unit step'''
-        return np.where(self.activation(x) >= 0.0,1,-1)
-
-
-
+#inport iris.data
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
 df.tail()
 
+#process iris data
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 x = df.iloc[0:100, [0, 2]].values
-plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
-plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
-            marker='x', label='versicolor')
-plt.xlabel('petal length')
-plt.ylabel('sepal length')
-plt.legend(loc='upper left')
-plt.show()
+
+'''
+show iris data
+'''
+# plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
+# plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
+#             marker='x', label='versicolor')
+# plt.xlabel('petal length')
+# plt.ylabel('sepal length')
+# plt.legend(loc='upper left')
+# plt.show()
 
 ppn = Perception(eta=0.1, n_iter=10)
 ppn.fit(x, y)
@@ -177,39 +118,9 @@ plt.xlabel('Epochs')
 plt.ylabel('Number of misclassifications')
 plt.show()
 
+
 # Plot_decision_regions(x,y,classifier=ppn)
 # plt.xlabel('sepal length [cm]')
 # plt.ylabel('petal length [cm]')
 # plt.legend(loc='upper left')
-# plt.show()
-
-
-# fig,ax = plt.subplots(nrows = 1,ncols = 2,figsize=(8,4))
-# ada1 = AdalineGD(n_iter=10,eta=0.01).fit(x,y)
-# ax[0].plot(range(1,len(ada1.cost_)+1),np.log10(ada1.cost_),marker='o')
-# ax[0].set_xlabel('Epochs')
-# ax[0].set_ylabel('log(Sum-squared-error)')
-# ax[0].set_title('Adaline - learning rate 0.01')
-# ada2 = AdalineGD(n_iter=50,eta=0.0001).fit(x,y)
-# ax[1].plot(range(1,len(ada2.cost_)+1),ada2.cost_,marker='o')
-# ax[1].set_xlabel('Epochs')
-# ax[1].set_ylabel('Sum-squared-error')
-# ax[1].set_title('Adaline - learning rate 0.0001')
-# # plt.show()
-
-# x_std = np.copy(x)
-# x_std[:,0] = (x[:,0] - x[:,0].mean()) / x[:,0].std()
-# x_std[:,1] = (x[:,1] - x[:,1].mean()) / x[:,1].std()
-# ada = AdalineGD(n_iter=15,eta=0.01)
-# ada.fit(x,y)
-# Plot_decision_regions(x_std,y,classifier=ada)
-# plt.title('Adaline - Gradient Descent')
-# plt.xlabel('sepal length [standardized]')
-# plt.ylabel('sepal length [standardized]')
-# plt.legend(loc='upper left')
-# # plt.show()
-
-# plt.plot(range(1,len(ada.cost_)+1),ada.cost_,marker='o')
-# plt.xlabel('Epochs')
-# plt.ylabel('Sum-squared-error')
 # plt.show()
