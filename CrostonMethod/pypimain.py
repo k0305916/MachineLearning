b1e3348d3337dc012ffe1c2683e8c502ec1a600b
Li <fan.li01@sap.com>
2020-11-06 17:40:35 +0800

    Add a test prcoess.

--- CrostonMethod/pypimain.py
+++ CrostonMethod/pypimain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import random
 from croston import croston
 import matplotlib.pyplot as plt
@@ -8,22 +9,29 @@ import matplotlib.pyplot as plt
 # val = np.array(random.sample(range(100,200), 10))
 # idxs = random.sample(range(50), 10)
 
-a = np.zeros(7)
-val = [1.0,4.0,5.0,3.0]
-idxs = [1,2-1,6-2,7-3]
+# a = np.zeros(7)
+# val = [1.0,4.0,5.0,3.0]
+# idxs = [1,2-1,6-2,7-3]
 
-ts = np.insert(a, idxs, val)
+# ts = np.insert(a, idxs, val)
 
-# fit_pred = croston.fit_croston(ts, 4, 'original') # croston's method
+input_data = pd.read_csv("./data/M4DataSet/NewYearly.csv")
+input_data = input_data.fillna(0)
+ts = input_data['Feature']
+
+fit_pred = croston.fit_croston(ts, 10, 'original') # croston's method
 
 # fit_pred = croston.fit_croston(ts, 4, 'sba') # Syntetos-Boylan approximation
-fit_pred = croston.fit_croston(ts, 10, 'sbj') # Shale-Boylan-Johnston
+# fit_pred = croston.fit_croston(ts, 10, 'sbj') # Shale-Boylan-Johnston
 
 
 yhat = np.concatenate([fit_pred['croston_fittedvalues'], fit_pred['croston_forecast']])
 
-print(ts)
-print(yhat)
+opt_model = fit_pred['croston_model']
+print("opt P: {0}   Q: {1}".format(opt_model["a_demand"],opt_model["a_interval"]))
+
+# print(ts)
+# print(yhat)
 
 plt.plot(ts)
 plt.plot(yhat)
