341e8a13c8c8d0aec367782c6cccb16a6c65478b
Li <fan.li01@sap.com>
2020-11-06 17:41:39 +0800

    Update.

--- ScipyOpt/main.py
+++ ScipyOpt/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import scipy.optimize
 import matplotlib.pyplot as plt
+# from operator import itemgetter
 
 def f(x):   # The rosenbrock function
     return .5*(1 - x[0])**2 + (x[1] - x[0]**2)**2
@@ -9,8 +10,16 @@ def jacobian(x):
     # return np.array((-2*.5*(1 - x[0]) - 4*x[0]*(x[1] - x[0]**2), 2*(x[1] - x[0]**2)))
     return np.array((-2*.5*(1 - x[0]), 2*(x[1] - x[0]**2)))
 
-result = scipy.optimize.minimize(f, [2, 2], method="L-BFGS-B", jac=None)
-print(result)
+# result = scipy.optimize.minimize(f, [2, 2], method='L-BFGS-B', jac=None)
+# print(result)
 
-result = scipy.optimize.brute(f,((-1, 2), (-1, 2)))
-print(result)
+# result = scipy.optimize.brute(f,((-1, 2), (-1, 2)))
+# print(result)
+
+opt_para = [{'para': (0,1), 'MSE':0.22},
+            {'para': (2,1), 'MSE':0.44},
+            {'para': (1,0), 'MSE':0.11}]
+
+# opt_para.sort(key=itemgetter('MSE'))
+opt_para = sorted(opt_para, key=lambda item: item['MSE'])
+print(opt_para)
