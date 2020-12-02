Upload correct formula.

ATA/main_doublepara_without_corss_and_forecast_ata.py
@@ -75,25 +75,8 @@ def _ata(
             epsilon
             ):
     
-    # ata decomposition
-    nzd = np.where(input_series != 0)[0] # find location of non-zero demand
-    
-    k = len(nzd)
-    z = input_series[nzd] # demand
-    
-    # initialize
-    x = np.concatenate([[nzd[0]], np.diff(nzd)]) # intervals
-    init = [z[0], np.mean(x)]
-    
-    zfit = np.array([None] * k)
-    xfit = np.array([None] * k)
-
-    # assign initial values and prameters
-    
-    zfit[0] = init[0]
-    xfit[0] = init[1]
-
-    correction_factor = 1
+    zfit = np.array([None] * input_series_length)
+    xfit = np.array([0] * input_series_length)
     
     if len(w) < 2:
         p = w[0]
@@ -103,40 +86,34 @@ def _ata(
         q = w[1]
     # fit model
     cc = []
-    nzd = np.delete(nzd, 0)
-    nzd = np.concatenate([nzd, [input_series_length]])
-    # a_demand = p / input_series_length
-    # a_interval = q / input_series_length
-    for i in range(0,k):
-        # if nzd[i] != 0:
-        a_demand = p / nzd[i]
-        a_interval = q / nzd[i]
+    cc.append(input_series[0])
+    j = 1
+    for i in range(0,input_series_length):
+        a_demand = p / j
+        a_interval = q / j
 
-        if i == 0:
-            xfit[i] = 0
-            zfit[i] = z[i]
+        # 提升效率的操作方式
+        if i <= p:
+            zfit[i] = input_series[i]
         else:
-            # 提升效率的操作方式
-            if nzd[i] <= p:
-                zfit[i] = z[i]
-            else:
-                # zfit[i] = zfit[i-1] + a_demand * (z[i] - zfit[i-1]) # demand
-                zfit[i] = a_demand * z[i] + (1-a_demand) * (zfit[i-1] + xfit[i-1])   #demand
+            # zfit[i] = zfit[i-1] + a_demand * (z[i] - zfit[i-1]) # demand
+            zfit[i] = a_demand * input_series[i] + (1-a_demand) * (zfit[i-1] + xfit[i-1])   #demand
 
 
         # for single exponential smoothing of ata
-        if q == 0:
-            xfit[i] = 0
-        else:
+        if q != 0:
             # for holt's linear trend method of ata
-            if i != 0:
-                if nzd[i] <= q:
-                    xfit[i] = z[i] - z[i-1]
-                else:
-                    xfit[i] = a_interval * (zfit[i] - zfit[i-1]) + (1-a_interval) * xfit[i-1] # interval
-                    # xfit[i] = xfit[i-1] + a_interval * (x[i] - xfit[i-1]) # interval
+            if i == 0:
+                xfit[i] = 0
+            elif i <= q: 
+                xfit[i] = input_series[i] - input_series[i-1]
+            else:
+                xfit[i] = a_interval * (zfit[i] - zfit[i-1]) + (1-a_interval) * xfit[i-1] # interval
+                # xfit[i] = xfit[i-1] + a_interval * (x[i] - xfit[i-1]) # interval
 
         cc.append(zfit[i] + xfit[i])
+        # print(zfit[i] + xfit[i])
+        j+=1
         
     # cc = correction_factor * zfit / (xfit + epsilon)
     
@@ -145,22 +122,11 @@ def _ata(
                         'a_interval':           q,
                         'demand_series':        pd.Series(zfit),
                         'interval_series':      pd.Series(xfit),
-                        'demand_process':       pd.Series(cc),
-                        'correction_factor':    correction_factor
+                        'demand_process':       pd.Series(cc)
                     }
     
     # calculate in-sample demand rate
-    
-    frc_in = np.zeros(input_series_length)
-    tv = np.concatenate([nzd, [input_series_length]]) # Time vector used to create frc_in forecasts
-
-    zfit_output = np.zeros(input_series_length)
-    xfit_output = np.zeros(input_series_length)
-    
-    for i in range(k):
-        frc_in[tv[i]:min(tv[i+1], input_series_length)] = cc[i]
-        zfit_output[tv[i]:min(tv[i+1], input_series_length)] = zfit[i]
-        xfit_output[tv[i]:min(tv[i+1], input_series_length)] = xfit[i]
+    frc_in = cc
 
     # forecast out_of_sample demand rate
     
@@ -181,8 +147,8 @@ def _ata(
         frc_out = []
         a_demand = p / input_series_length
         a_interval = q / input_series_length
-        zfit_frcout = a_demand * z[-1] + (1-a_demand)*(zfit_output[-1] + xfit_output[-1])
-        xfit_frcout = a_interval * (zfit_frcout - zfit_output[-1]) + (1-a_interval)*xfit_output[-1]
+        zfit_frcout = a_demand * input_series[-1] + (1-a_demand)*(zfit[-1] + xfit[-1])
+        xfit_frcout = a_interval * (zfit_frcout - zfit[-1]) + (1-a_interval)*xfit[-1]
 
 
         for i in range(1,h+1):
@@ -197,7 +163,7 @@ def _ata(
                             'model':                    ata_model,
                             'in_sample_forecast':       frc_in,
                             'out_of_sample_forecast':   frc_out,
-                            'fit_output':               zfit_output
+                            'fit_output':               cc
                         }
     
     return return_dictionary
@@ -214,6 +180,7 @@ def _ata_opt(
     # init_p = np.random.randint(1, input_series_length)
     # init_q = np.random.randint(0, init_p)
     # p0 = np.array([init_p,init_q])
+    # p0 = np.array([1.0,0.0])
     # pbounds = ((1, input_series_length), (0, input_series_length))
 
 
@@ -224,16 +191,16 @@ def _ata_opt(
     # # # # 感觉可以深挖下这个的算法耶。。里面还含有分布函数的选择。
     # # # # 传入梯度下降的公式，则可以降低计算的消耗。。。。
     # # # # 调整步长，来修正梯度下降的效率？
-    wopt = minimize(
-                        fun = _ata_cost, 
-                        x0 = p0, 
-                        method='L-BFGS-B',
-                        bounds=pbounds,
-                        args=(input_series, input_series_length, epsilon)
-                    )
+    # wopt = minimize(
+    #                     fun = _ata_cost, 
+    #                     x0 = p0, 
+    #                     method='L-BFGS-B',
+    #                     bounds=pbounds,
+    #                     args=(input_series, input_series_length, epsilon)
+    #                 )
 
-    constrained_wopt = wopt.x
-    fun = wopt.fun
+    # constrained_wopt = wopt.x
+    # fun = wopt.fun
 
 
     # # p0 = np.array([1])
@@ -249,13 +216,13 @@ def _ata_opt(
     # fun = wopt.fun
 
 
-    # pbounds = ((1, input_series_length), (0, input_series_length))
-    # wopt = scipy.optimize.brute(_ata_cost,pbounds,
-    #                             args=(input_series, input_series_length, epsilon))
+    pbounds = ((1, input_series_length), (0, input_series_length))
+    wopt = scipy.optimize.brute(_ata_cost,pbounds,
+                                args=(input_series, input_series_length, epsilon))
     
-    # # constrained_wopt = np.minimum([1], np.maximum([0], wopt.x))
-    # constrained_wopt = wopt
-    # fun = 0
+    # constrained_wopt = np.minimum([1], np.maximum([0], wopt.x))
+    constrained_wopt = wopt
+    fun = 0
 
     # # 双重退火
     # pbounds = ((1, input_series_length), (0, input_series_length))
@@ -294,7 +261,8 @@ def _ata_cost(
                         )['in_sample_forecast']
 
     # MSE： 在该算法中，optimize时，MSE（RMSE）并不是一个好的选择。-------------------------------------
-    E = input_series - frc_in
+    frc_in.pop()
+    # E = input_series - frc_in
 
     # # 变形MSE
     # # count = min(input_series_length-1,(int)(p0[0]))
@@ -302,14 +270,23 @@ def _ata_cost(
     # # outdata = frc_in[count:]
     # # E = indata - outdata
     
+    # standard MSE
     # E = E[E != np.array(None)]
-    # # E = np.sqrt(np.mean(E ** 2))
     # E = np.mean(E ** 2)
 
-    # # MAPE 针对非0数据的话，效果会比较好--------------------------------
-    E = np.abs((frc_in - input_series)) / (np.abs(input_series) + 1e-7)
-    E = E.sum() / input_series_length
-    # E = E.sum() / len(input_series)
+    # # standard RMSE
+    # E = E[E != np.array(None)]
+    # E = np.sqrt(np.mean(E ** 2))
+
+    # # # # MAPE 针对非0数据的话，效果会比较好--------------------------------
+    # E = np.abs((frc_in - input_series)) / (np.abs(input_series) + 1e-7)
+    # E = E.sum() / input_series_length
+
+    # PAL MAPE
+    E = np.abs((frc_in - input_series))
+    up = E.sum()
+    low = input_series.sum()
+    E = up / low
 
     # print(("count: {0}  p: {1}  q: {2}  E: {3}").format(count, p0[0], p0[1], E))
     if len(p0) < 2:
@@ -324,10 +301,10 @@ def _ata_cost(
 # ts = np.insert(a, idxs, val)
 
 
-# # Yearly dataset
-# input_data = pd.read_csv("./data/M4DataSet/NewYearly.csv")
-# input_data = input_data.fillna(0)
-# ts = input_data['Feature']
+# Yearly dataset
+input_data = pd.read_csv("./data/M4DataSet/NewYearly.csv")
+input_data = input_data.fillna(0)
+ts = input_data['Feature']
 # ts = input_data['Feature'][:1000]
 
 # fit_pred = fit_ata(ts, 4) # ata's method
@@ -335,16 +312,16 @@ def _ata_cost(
 # yhat = np.concatenate([fit_pred['ata_fittedvalues'], fit_pred['ata_forecast']])
 # yhat = fit_pred['ata_demand_series']
 
-#excel dataset
-ts = [
-362.35, 361.51, 363.51, 362.56, 361.88, 361.63, 361.35, 362.82, 360.64, 362.35, 362.77, 361.79, 
-361.41, 360.35, 357.75, 356.11, 355.24, 353.58, 347.49, 333.02, 0, 331.26, 322.03, 314.66, 
-312.74, 307.52, 304.87, 301.73, 300.62, 303.82, 307.40, 309.85, 311.06, 312.83, 314.97, 318.79, 
-320.81, 323.62, 325.66, 331.44, 332.61, 335.44, 336.57, 338.26, 337.20, 338.30, 342.17, 342.40, 
-341.63, 344.27, 342.50, 343.39, 343.57, 346.44, 347.07, 347.47, 349.21, 349.56, 352.03, 353.95, 
-355.17, 352.91, 356.45, 358.93, 362.35, 361.51, 363.51, 362.56, 361.88, 361.63, 361.35, 362.82, 
-360.64, 362.35, 362.77, 322.03, 314.66, 312.74, 307.52, 304.87, 301.73, 300.62
-]
+# # excel dataset
+# ts = [
+# 362.35, 361.51, 363.51, 362.56, 361.88, 361.63, 361.35, 362.82, 360.64, 362.35, 362.77, 361.79, 
+# 361.41, 360.35, 357.75, 356.11, 355.24, 353.58, 347.49, 333.02, 0, 331.26, 322.03, 314.66, 
+# 312.74, 307.52, 304.87, 301.73, 300.62, 303.82, 307.40, 309.85, 311.06, 312.83, 314.97, 318.79, 
+# 320.81, 323.62, 325.66, 331.44, 332.61, 335.44, 336.57, 338.26, 337.20, 338.30, 342.17, 342.40, 
+# 341.63, 344.27, 342.50, 343.39, 343.57, 346.44, 347.07, 347.47, 349.21, 349.56, 352.03, 353.95, 
+# 355.17, 352.91, 356.45, 358.93, 362.35, 361.51, 363.51, 362.56, 361.88, 361.63, 361.35, 362.82, 
+# 360.64, 362.35, 362.77, 322.03, 314.66, 312.74, 307.52, 304.87, 301.73, 300.62
+# ]
 
 # ts = [
 #     0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
@@ -396,7 +373,7 @@ ts = [
 
 
 
-# # # single process
+# # single process
 # fit_pred = _ata(
 #                 input_series = np.asarray(ts), 
 #                 input_series_length = len(ts),

ScipyOpt/main.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as plt
 # from operator import itemgetter
 
 def f(x):   # The rosenbrock function
+    print(("x0: {0}, x1: {1}").format(x[0],x[1]))
     return .5*(1 - x[0])**2 + (x[1] - x[0]**2)**2
 def jacobian(x):
-    # return np.array((-2*.5*(1 - x[0]) - 4*x[0]*(x[1] - x[0]**2), 2*(x[1] - x[0]**2)))
-    return np.array((-2*.5*(1 - x[0]), 2*(x[1] - x[0]**2)))
+    return np.array((-2*.5*(1 - x[0]) - 4*x[0]*(x[1] - x[0]**2), 2*(x[1] - x[0]**2)))
+    # return np.array((-2*.5*(1 - x[0]), 2*(x[1] - x[0]**2)))
 
-# result = scipy.optimize.minimize(f, [2, 2], method='L-BFGS-B', jac=None)
-# print(result)
+result = scipy.optimize.minimize(f, [2, 2], method='L-BFGS-B', jac=None)
+# result = scipy.optimize.minimize(f, [2, 2], method='L-BFGS-B', jac=jacobian)
+print(result)
 
 # result = scipy.optimize.brute(f,((-1, 2), (-1, 2)))
 # print(result)
 
-opt_para = [{'para': (0,1), 'MSE':0.22},
-            {'para': (2,1), 'MSE':0.44},
-            {'para': (1,0), 'MSE':0.11}]
+# opt_para = [{'para': (0,1), 'MSE':0.22},
+#             {'para': (2,1), 'MSE':0.44},
+#             {'para': (1,0), 'MSE':0.11}]
 
-# opt_para.sort(key=itemgetter('MSE'))
-opt_para = sorted(opt_para, key=lambda item: item['MSE'])
-print(opt_para)
+# # opt_para.sort(key=itemgetter('MSE'))
+# opt_para = sorted(opt_para, key=lambda item: item['MSE'])
+# print(opt_para)